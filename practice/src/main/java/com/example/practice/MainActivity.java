2f41712cb3c54958519e7684f50561802ef2800e
Maksym <m5732005@gmail.com>
2016-10-02 20:14:01 +0300

    clear project Practice

--- practice/src/main/java/com/example/practice/MainActivity.java
+++ practice/src/main/java/com/example/practice/MainActivity.java
@@ -9,31 +9,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText mUserName;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mUserName = (EditText) findViewById(R.id.edit_box_field);
     }
 
-    public void onClick(View view) {
-        switch (view.getId())
-        {
-            case R.id.ok_button:
-                String text = mUserName.getText().toString().trim();
-                if (text.length() > 0)
-                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-                mUserName.setText(null);
-                break;
-            case R.id.cancel_button:
-                mUserName.setText(null);
-                break;
-            default:
-                Toast.makeText(this, "unknown command", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-    }
 }
