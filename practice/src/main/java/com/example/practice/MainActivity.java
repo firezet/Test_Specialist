f8a9558f45152066f3dc8395666e0316b61d2fc3
Maksym <m5732005@gmail.com>
2016-10-02 12:09:54 +0300

    rename practice to delete + practice project

--- practice/src/main/java/com/example/practice/MainActivity.java
+++ practice/src/main/java/com/example/practice/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.practice;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onClick(View view) {
     }
 }
