268b62d33b82f7afc1bead402099915bc2729c89
Maksym <m5732005@gmail.com>
2016-10-03 17:13:57 +0300

    clear project Practice 2

--- practice/src/main/java/com/example/practice/MainActivity.java
+++ practice/src/main/java/com/example/practice/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.practice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
