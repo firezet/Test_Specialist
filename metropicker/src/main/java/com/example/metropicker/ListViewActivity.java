58ee8b7ae36b331b03b5c6a6674c10e5fd599fbb
Maksym <m5732005@gmail.com>
2016-10-05 19:33:09 +0300

    new project MetroPicker

---
+++ metropicker/src/main/java/com/example/metropicker/ListViewActivity.java
@@ -0,0 +1,36 @@
+package com.example.metropicker;
+
+import android.app.ListActivity;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ListViewActivity extends ListActivity implements AdapterView.OnItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Resources resources = getResources();
+        String [] arrayStations = resources.getStringArray(R.array.stations);
+
+        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String>(this,
+                R.layout.list_item, arrayStations);
+
+        getListView().setAdapter(arrayAdapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this, ((TextView) view).getText() + " p" + String.valueOf(i),
+                Toast.LENGTH_SHORT).show();
+
+    }
+}
