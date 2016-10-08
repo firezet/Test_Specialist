b2a6a92ad566b0088306c57f86d3613e0eccbea9
Maksym <m5732005@gmail.com>
2016-10-08 10:13:09 +0300

    before activity result back

--- saveformetropicker/src/main/java/com/example/saveformetropicker/MainActivity.java
+++ saveformetropicker/src/main/java/com/example/saveformetropicker/MainActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int HOLODNOGORSKAYA = 1;
+    private static final short STATION_SELECT = 1;
+    private int mSelectedRadioButton = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +19,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ButtonSelectTheStation(View view) {
-        //Intent intent = new Intent(this, HolodnogorskayaActivity.class);
         Intent intent = new Intent(this, StationList.class);
-        intent.putExtra("RQ", 1);
+        if ( mSelectedRadioButton == R.id.rbutton_holodnogorskaya
+                || mSelectedRadioButton == 0)
+            intent.putExtra("RQ", 1);
+
+        if ( mSelectedRadioButton == R.id.rbutton_alekseevskaya )
+            intent.putExtra("RQ", 2);
+
+        if ( mSelectedRadioButton == R.id.rbutton_saltovskaya )
+            intent.putExtra("RQ", 3);
+
         if ( intent.resolveActivity(getPackageManager()) != null )
-            startActivityForResult(intent, HOLODNOGORSKAYA);
+            startActivityForResult(intent, STATION_SELECT);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+    }
+
+    public void onRButtonClicked(View view) {
+        RadioButton radioButton = (RadioButton) view;
+        mSelectedRadioButton = radioButton.getId();
+        Toast.makeText(this, radioButton.getText(), Toast.LENGTH_SHORT).show();
     }
 }
