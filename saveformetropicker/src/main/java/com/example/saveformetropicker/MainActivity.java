5d051b120fd55eb4b6a02757dfc34db2c275628a
Maksym <m5732005@gmail.com>
2016-10-09 19:47:19 +0300

    + restored function of station

--- saveformetropicker/src/main/java/com/example/saveformetropicker/MainActivity.java
+++ saveformetropicker/src/main/java/com/example/saveformetropicker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.saveformetropicker;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final short STATION_SELECT = 1;
     private int mSelectedRadioButton = 0;
+
+    private static final String PREFS = "PREFS";
+    static final String KEY_STATION = "selectedStation";
+
+    private SharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +44,36 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        SharedPreferences.Editor editor = prefs.edit();
+
         if ( resultCode == RESULT_OK) {
             String stationNameResult = data.getStringExtra("ResultIntent");
             ((TextView) findViewById(R.id.selected_station_text_show))
-                    .setText(stationNameResult);
+                .setText(stationNameResult);
+
+            prefs = getSharedPreferences(PREFS, MODE_PRIVATE);
+            editor.putString(KEY_STATION,stationNameResult);
+            editor.apply();
         } else {
             super.onActivityResult(requestCode, resultCode, data);
             Toast.makeText(this, "Station is not selected", Toast.LENGTH_SHORT).show();
+            editor.remove(KEY_STATION);
+            editor.apply();
         }
     }
 
     public void ButtonLastStationLoad (View view) {
-        Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
+        prefs = getSharedPreferences(PREFS, MODE_PRIVATE);
+        String selectedStation = prefs.getString(KEY_STATION, null);
+        if ( selectedStation != null ) {
+            ((TextView) findViewById(R.id.selected_station_text_show))
+                    .setText(selectedStation);
+            Toast.makeText(this, "restored from prefs", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, selectedStation, Toast.LENGTH_SHORT).show();
+        } else {
+            ((TextView) findViewById(R.id.selected_station_text_show))
+                    .setText("no saved stations");
+        }
     }
 
     public void onRButtonClicked(View view) {
