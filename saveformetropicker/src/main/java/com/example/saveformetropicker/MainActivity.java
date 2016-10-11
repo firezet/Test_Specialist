ee47b3740d00a8ce6611df4de1514687210bbf77
Maksym <m5732005@gmail.com>
2016-10-11 12:33:56 +0300

    + save and load RadioButton last position

--- saveformetropicker/src/main/java/com/example/saveformetropicker/MainActivity.java
+++ saveformetropicker/src/main/java/com/example/saveformetropicker/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,61 +15,68 @@ public class MainActivity extends AppCompatActivity {
     private int mSelectedRadioButton = 0;
 
     Storage mStorage;
+    RadioGroup mRadioGroup;
 
     @Override
-    protected void onCreate ( Bundle savedInstanceState ) {
-        super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_main );
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
 
-        mStorage = new Storage ( this );
+        mStorage = new Storage (this);
+
+        mRadioGroup = (RadioGroup) findViewById (R.id.radio_button_group);
     }
 
-    public void ButtonSelectTheStation ( View view ) {
-        Intent intent = new Intent ( this, StationList.class );
+    public void ButtonSelectTheStation (View view) {
+        Intent intent = new Intent (this, StationList.class);
         if ( mSelectedRadioButton == R.id.rbutton_holodnogorskaya
                 || mSelectedRadioButton == 0 )
-            intent.putExtra ( "RQ", 1 );
+            intent.putExtra ("RQ", 1);
 
         if ( mSelectedRadioButton == R.id.rbutton_alekseevskaya )
-            intent.putExtra ( "RQ", 2 );
+            intent.putExtra ("RQ", 2);
 
         if ( mSelectedRadioButton == R.id.rbutton_saltovskaya )
-            intent.putExtra ( "RQ", 3 );
+            intent.putExtra ("RQ", 3);
 
-        if ( intent.resolveActivity ( getPackageManager () ) != null )
-            startActivityForResult ( intent, STATION_SELECT );
+        if ( intent.resolveActivity (getPackageManager ()) != null )
+            startActivityForResult (intent, STATION_SELECT);
     }
 
     @Override
-    protected void onActivityResult ( int requestCode, int resultCode, Intent data ) {
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
         if ( resultCode == RESULT_OK ) {
-            String stationNameResult = data.getStringExtra ( "ResultIntent" );
-            ( (TextView) findViewById ( R.id.selected_station_text_show ) )
-                    .setText ( stationNameResult );
+            String stationNameResult = data.getStringExtra ("ResultIntent");
+            ( (TextView) findViewById (R.id.selected_station_text_show) )
+                    .setText (stationNameResult);
 
-            mStorage.savePrefs ( data );
+            mStorage.savePrefs (data);
         } else {
-            super.onActivityResult ( requestCode, resultCode, data );
-            Toast.makeText ( this, "Station is not selected", Toast.LENGTH_SHORT ).show ();
+            super.onActivityResult (requestCode, resultCode, data);
+            Toast.makeText (this, "Station is not selected", Toast.LENGTH_SHORT).show ();
 
-            mStorage.clearKey();
+            mStorage.savePrefs (null);
         }
     }
 
-    public void ButtonLastStationLoad ( View view ) {
-        TextView fieldWithSelectedStation = (TextView) findViewById ( R.id.selected_station_text_show );
-        mStorage.loadPref ( fieldWithSelectedStation );
+    public void ButtonLastStationLoad (View view) {
+        TextView fieldWithSelectedStation = (TextView) findViewById (R.id.selected_station_text_show);
+        mStorage.loadStationField (fieldWithSelectedStation);
+        mStorage.loadRadioButton (mRadioGroup);
     }
 
-    public void onRButtonClicked ( View view ) {
+    public void onRButtonClicked (View view) {
         RadioButton radioButton = (RadioButton) view;
         mSelectedRadioButton = radioButton.getId ();
-        Toast.makeText ( this, radioButton.getText (), Toast.LENGTH_SHORT ).show ();
+        Toast.makeText (this, radioButton.getText (), Toast.LENGTH_SHORT).show ();
+
+        // save part
+        mStorage.saveRadioButton (mRadioGroup.indexOfChild (radioButton));
     }
 
     @Override
     protected void onDestroy () {
         super.onDestroy ();
-        Toast.makeText ( this, "onDestroy", Toast.LENGTH_SHORT ).show ();
+        Toast.makeText (this, "onDestroy", Toast.LENGTH_SHORT).show ();
     }
 }
--- saveformetropicker/src/main/java/com/example/saveformetropicker/Storage.java
+++ saveformetropicker/src/main/java/com/example/saveformetropicker/Storage.java
@@ -3,6 +3,8 @@ package com.example.saveformetropicker;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,35 +16,50 @@ class Storage {
 
     private static final String PREFS = "PREFS";
     private static final String KEY_STATION = "STATION";
+    private static final String KEY_RADIO_BUTTON_INDEX = "RADIO_BUTTON";
 
     private Context mContext;
     private SharedPreferences mPrefs;
 
-    Storage ( Context context ) {
+    Storage (Context context) {
         mContext = context;
-        mPrefs = context.getSharedPreferences ( PREFS, Context.MODE_PRIVATE );
+        mPrefs = context.getSharedPreferences (PREFS, Context.MODE_PRIVATE);
     }
 
-    void savePrefs ( Intent data ) {
+    void savePrefs (Intent data) {
         SharedPreferences.Editor editor = mPrefs.edit ();
-        editor.putString ( KEY_STATION, data.getStringExtra ( "ResultIntent" ) );
-        Toast.makeText ( mContext, "save + " + data.getStringExtra ( "ResultIntent" ), Toast.LENGTH_SHORT ).show ();
-        editor.apply ();
-    }
-
-    void loadPref (TextView field) {
-        String selectedStation = mPrefs.getString ( KEY_STATION, null );
-        if ( selectedStation != null ) {
-            field.setText ( selectedStation );
-            Toast.makeText ( mContext, selectedStation, Toast.LENGTH_SHORT ).show ();
+        if ( data == null ) {
+            // можно удалить ключ или записать новое значение. null андроид обработает быстрее
+            // поскольку не будет сравнивать то что там будет, а проверит наличие ключа.
+            editor.remove (KEY_STATION);
+            editor.apply ();
         } else {
-            field.setText ( R.string.no_saved_stations );
+            editor.putString (KEY_STATION, data.getStringExtra ("ResultIntent"));
+            editor.apply ();
         }
     }
 
-    void clearKey () {
+    void loadStationField (TextView field) {
+        String selectedStation = mPrefs.getString (KEY_STATION, null);
+        if ( selectedStation != null ) {
+            field.setText (selectedStation);
+            Toast.makeText (mContext, selectedStation, Toast.LENGTH_SHORT).show ();
+        } else {
+            field.setText (R.string.no_saved_stations);
+        }
+    }
+
+    void saveRadioButton (int rbIndex) {
         SharedPreferences.Editor editor = mPrefs.edit ();
-        editor.remove ( KEY_STATION );
+        editor.putInt (KEY_RADIO_BUTTON_INDEX, rbIndex);
         editor.apply ();
+        Toast.makeText (mContext, String.valueOf (rbIndex), Toast.LENGTH_SHORT).show ();
+    }
+
+    void loadRadioButton (RadioGroup radioButtonGroup) {
+        int savedRadioButtonIndex = mPrefs.getInt (KEY_RADIO_BUTTON_INDEX, 0);
+        RadioButton radioButton = (RadioButton) radioButtonGroup
+                                                .getChildAt (savedRadioButtonIndex);
+        radioButton.setChecked (true);
     }
 }
