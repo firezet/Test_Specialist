5840246161b7c78f33e045685d1b2a0e840b3669
Maksym Galushka <m5732005@gmail.com>
2016-10-26 22:04:15 +0300

    + add SQLite DB to Note project;

--- note/src/main/java/com/example/note/MainActivity.java
+++ note/src/main/java/com/example/note/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.note;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -9,8 +11,18 @@ import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String[] FIELDS = {"_id", DBOpenHelper.COLUMN_NOTE,};
+    private static final String[] FROM = {DBOpenHelper.COLUMN_NOTE,};
+    private static final int[] TO = {R.id.noteTextView,};
+    private static final java.lang.String ORDER = "_id DESC";
+
+
     EditText mInputField;
     ListView mNotesList;
+
+    DBOpenHelper mDBOpenHelper = new DBOpenHelper (this);
+    SQLiteDatabase mDB;
+
     SimpleCursorAdapter mAdapter;
 
     @Override
@@ -20,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
 
         mInputField = (EditText) findViewById (R.id.inputField);
         mNotesList = (ListView) findViewById (R.id.notesList);
+        mAdapter = new SimpleCursorAdapter (this, R.layout.note,
+                null, FROM, TO, 0);
+        mNotesList.setAdapter (mAdapter);
+    }
+
+    @Override
+    protected void onResume () {
+        super.onResume ();
+        mDB = mDBOpenHelper.getWritableDatabase ();
+        Cursor cursor = mDB.query (mDBOpenHelper.DB_TABLE,
+                FIELDS,
+                null, null, null, null,
+                ORDER);
+        mAdapter.swapCursor (cursor);
     }
 
     public void onButtonClick (View view) {
