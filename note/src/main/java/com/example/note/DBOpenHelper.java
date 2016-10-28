30377dd0f4dcfaccc0969f84735836504546394d
Maksym Galushka <m5732005@gmail.com>
2016-10-28 20:35:38 +0300

    add DBOpenHelper.java

---
+++ note/src/main/java/com/example/note/DBOpenHelper.java
@@ -0,0 +1,28 @@
+package com.example.note;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Maksym Galushka on 28.10.2016.
+ */
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+    public DBOpenHelper (Context context,
+                         String name,
+                         SQLiteDatabase.CursorFactory factory,
+                         int version) {
+        super (context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate (SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
