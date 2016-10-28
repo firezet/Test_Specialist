ab621aeb343db23dd39f98fe700cad25861c4cf9
Maksym Galushka <m5732005@gmail.com>
2016-10-28 20:37:19 +0300

    change3 activity_main.xml ch_1 MainActivity.java

--- note/src/main/java/com/example/note/MainActivity.java
+++ note/src/main/java/com/example/note/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.note;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mInputField;
+    ListView mNotesList;
+
+    SimpleCursorAdapter mSimpleCursorAdapter;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        mInputField = (EditText) findViewById (R.id.inputField);
+        mNotesList = (ListView) findViewById (R.id.notesList);
+    }
+
+    public void onOkButtonClick (View view) {
     }
 }
