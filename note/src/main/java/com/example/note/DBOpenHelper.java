349557e71479ba8bbf0284e3b2d67ff326afac9d
Maksym Galushka <m5732005@gmail.com>
2016-10-28 21:01:39 +0300

    ch_1 DBOpenHelper.java

--- note/src/main/java/com/example/note/DBOpenHelper.java
+++ note/src/main/java/com/example/note/DBOpenHelper.java
@@ -1,5 +1,6 @@
 package com.example.note;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,18 +10,41 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DBOpenHelper extends SQLiteOpenHelper {
-    public DBOpenHelper (Context context,
-                         String name,
-                         SQLiteDatabase.CursorFactory factory,
-                         int version) {
-        super (context, name, factory, version);
+
+    private static final String DB_NAME = "notes.db";
+    private static final int DB_VERSION = 1;
+    private static final String DB_TABLE = "notes";
+    private static final String COLUMN_NOTE = "note";
+    private static final String DB_CREATE = "create table"
+            + DB_TABLE
+            + "(_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + COLUMN_NOTE
+            + "TEXT NOT NULL);";
+
+    private Context mContext;
+
+
+    public DBOpenHelper (Context context)
+        /*,String name,SQLiteDatabase.CursorFactory factory,int version)*/ {
+        super (context, DB_NAME, null, DB_VERSION);
+        mContext = context;
     }
 
     @Override
     public void onCreate (SQLiteDatabase db) {
+        db.execSQL (DB_CREATE);
 
+        // TODO DELETE AFTER DEBUGGING
+        // param '1' in ContentValues is how many columns create in start.
+        // if null - auto create ~ 50.
+        ContentValues values = new ContentValues (1);
+        for ( int i = 1; i < 6; i++ ) {
+            values.put (COLUMN_NOTE, "Note #" + i);
+            db.insert (DB_TABLE, null, values);
+        }
     }
 
+    // when new version != ald version
     @Override
     public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion) {
 
